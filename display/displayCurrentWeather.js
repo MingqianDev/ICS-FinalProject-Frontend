cc0c5b3c678eaa47da1c0c2ea78e8d0d9299f014
kyleCao1 <126813426+kyleCao1@users.noreply.github.com>
2023-06-06 14:40:25 +0000

    update replit

--- display/displayCurrentWeather.js
+++ display/displayCurrentWeather.js
@@ -1,5 +1,5 @@
 export function displayCurrentWeather(data){
     document.querySelector('#temperature').textContent = data.temperature + '°C';
     document.querySelector('#weather').textContent = data.weather;
-    document.querySelector('#weather-icon').src = `../icon/02d.png`;
+    document.querySelector('#weather-icon').src = `../icon/${data.icon}.png`;
 }
